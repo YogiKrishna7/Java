2366a3a752b0d4c19dedd3b0d0ee047c4a66552c
Yogendra <120395997+YogiKrishna7@users.noreply.github.com>
2024-09-25 00:00:55 +0530

    Updated

--- Basics/StringReverse.java
+++ Basics/StringReverse.java
@@ -1,12 +1,17 @@
-
 public class StringReverse {
 
     public static void main(String[] args) {
-        String name = "yogi";
-        String rev_str = "";
+        String name = "yogi"; // Declare a string variable to store the original string
+
+        String rev_str = ""; // Initialize an empty string to store the reversed string
+
+        // Iterate through the original string from the last character to the first
         for (int i = name.length() - 1; i >= 0; i--) {
+            // Append the current character to the reversed string
             rev_str += name.charAt(i);
         }
+
+        // Print the reversed string
         System.out.println("Reverse String: " + rev_str);
     }
 }
