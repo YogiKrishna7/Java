da6144f3cd7f01a333d3c5fe206e3cbba31f8038
YogiKrishna7 <120395997+YogiKrishna7@users.noreply.github.com>
2024-10-15 15:34:08 +0530

    Updated

--- Basics/PrimeNumsInRange.java
+++ Basics/PrimeNumsInRange.java
@@ -4,8 +4,8 @@ public class PrimeNumsInRange {
         for (int i = 2; i <= range; i++) {
             boolean flag = true;
 
-            for (int j = 2; j <= i; j++) {
-                if (i % j == 0 && i % 2 == 0) {
+            for (int j = 2; j  <= i/2; j++) {
+                if (i % j == 0) {
                     flag = false;
                     break;
                 }
