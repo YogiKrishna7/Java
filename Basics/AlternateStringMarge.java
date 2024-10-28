4a8a4b0202b8ef389e43037d6afb32a493dcdeee
YogiKrishna7 <120395997+YogiKrishna7@users.noreply.github.com>
2024-10-28 23:55:55 +0530

    Created

---
+++ Basics/AlternateStringMarge.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        char s[] = word1.toCharArray();
+        char t[] = word2.toCharArray();
+        String word3 = "";
+        for (int i = 0; i < s.length(); i++) {
+            word3 += s[i] + "" + t[i];
+        }
+        System.out.println(word3);
+        return word3;
+        
+    }
+
+    public static void main(String[] a) {
+        Solution s = new Solution();
+        s.mergeAlternately("abc", "pqr");
+    }
+}
